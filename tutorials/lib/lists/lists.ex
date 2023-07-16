810d5941682b7b04687013e273d2de4db9809a84
Sipho Ndlalane <siphonhata@gmail.com>
2023-07-16 20:08:54 +0200

    Flat_maps and concatenating lists

--- tutorials/lib/lists/lists.ex
+++ tutorials/lib/lists/lists.ex
@@ -40,5 +40,20 @@ defmodule Tutorials.Lists do
     def map([], _, acc), do: acc |> reverse()
     def map([h | t], func, acc), do: map(t, func, [func.(h) | acc])
 
+
+    # Concat
+
+    @spec concat([any()], [any()]) :: [any()]
+    def concat(src, dst), do: concat_func(src |> reverse(), dst)
+
     
+    defp concat_func([], dst), do: dst
+    defp concat_func([h | t], dst), do: concat_func(t, [h | dst])
+
+
+    # FlatMap
+
+    def flat_map(elements, func, acc \\ [])
+    def flat_map([], _, acc), do: acc
+    def flat_map([h | t], func, acc), do: flat_map(t, func, concat(acc, func.(h)))
 end
