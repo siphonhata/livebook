d5e524850957b8f7150e974f203e0cb65321d3e0
Sipho Ndlalane <siphonhata@gmail.com>
2023-07-16 19:44:35 +0200

    Working with lists and module documentation

---
+++ tutorials/lib/lists/lists.ex
@@ -0,0 +1,38 @@
+defmodule Tutorials.Lists do
+
+    @moduledoc """
+    Function summary:
+        1. sum
+    """
+    @doc """
+        Returns the sum of numbers in a list.
+    """
+
+    @spec sum(list(number())) :: number()
+    def sum(nums), do: sum_tail_rec(nums)
+
+    @spec sum_simple(list(number())) :: number()
+    def sum_simple([]), do: 0
+    def sum_simple([h | t]) do
+        h + sum_simple(t)
+    end
+
+    @spec sum_tail_rec(list(number()), integer()) :: number()
+    def sum_tail_rec(nums, acc \\ 0)
+    def sum_tail_rec([], acc), do: acc
+    def sum_tail_rec([h | t], acc), do: sum_tail_rec(t, acc + h)
+
+
+    # Reverse list
+
+    @spec reverse(list(any), [any()]) :: [any()] #Specification of how the function look like
+    def reverse(elements, acc \\ [])
+    def reverse([], acc), do: acc
+    def reverse([h | t], acc) do
+        reverse(t, [h | acc])
+    end
+
+
+
+   
+end
