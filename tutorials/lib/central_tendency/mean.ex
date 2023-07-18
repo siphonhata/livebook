1087c2be3574590d64fd1abcaf69bb3a15bb63ab
Sipho Ndlalane <siphonhata@gmail.com>
2023-07-18 09:27:24 +0200

    Stats project

---
+++ tutorials/lib/central_tendency/mean.ex
@@ -0,0 +1,24 @@
+defmodule Stats.CentralTendency.Mean do
+
+  alias Stats.{Errors, Validators}
+
+
+  def population_mean([]), do: Errors.invalid_data_type()
+
+  def population_mean when is_list(nums) do
+    nums
+    |> Validators.validate_num_list()
+    |> calc_population_mean()
+  end
+
+
+
+  def calc_population_mean({false, _}), do: Errors.invalid_data_type()
+  def calc_population_mean({true, nums}) do
+    nums
+    |> Enum.sum()
+    |> mean(Enum.count(nums))
+  end
+
+  def mean(sigma, count), do: sigma / count
+end
---
+++ tutorials/lib/errors.ex
@@ -0,0 +1,3 @@
+defmodule Stats.Errors do
+  def invalid_data_type(), do: {:error, "Invalid data type"}
+end
